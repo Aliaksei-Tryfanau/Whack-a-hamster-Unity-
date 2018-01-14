be0171db58a73dbde7d629eb01496fe2304ff6a2
Arracias <arracias@github.com>
2018-01-14 22:35:31 +0300

    Core mechanic

---
+++ Assets/Scripts/HamsterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HamsterController : MonoBehaviour {
+
+    public List<HamsterMovement> hamstersList;
+    public float timer;
+    static public float godTimer;
+    bool gameIsRunning = true;
+    static public float delayTop;
+    static public float delayBottom;
+    public float setDelayTop;
+    public float setDelayBottom;
+    public Text scoreText;
+    public static int score;
+
+
+    void Start () {
+        godTimer = timer;
+        delayTop = setDelayTop;
+        delayBottom = setDelayBottom;
+    }
+	
+	void Update () {
+        for (int i = 0; i < hamstersList.Count; i++)
+        {
+            if (gameIsRunning == true)
+                    hamstersList[i].Move();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            gameIsRunning = !gameIsRunning;
+
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
---
+++ Assets/Scripts/HamsterMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HamsterMovement : MonoBehaviour
+{
+
+    public float limitations_y;
+    public float speed_y;
+    float currentTime;
+    bool timeToStop = false;
+    float timer;
+    Vector3 newPos;
+    bool hamsterIsClickable = true;
+
+    void Start()
+    {
+
+    }
+
+    public void Move()
+    {
+        newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        if (timeToStop == false)
+        {
+            transform.position = newPos + new Vector3(0.0f, speed_y, 0.0f) * Time.deltaTime;
+        }
+
+        if (transform.position.y >= limitations_y)
+        {
+            StopAtTop();
+        }
+
+        if (transform.position.y <= -limitations_y)
+        {
+            StopAtBottom();
+            if (hamsterIsClickable == false)
+                hamsterIsClickable = true;
+        }
+
+        currentTime = Time.time;
+        if (currentTime > HamsterController.godTimer && timeToStop == false)
+        {
+            timeToStop = true;
+        }
+    }
+
+    public void StopAtTop()
+    {
+        timer += Time.deltaTime;
+        timeToStop = true;
+        if (timer >= HamsterController.delayTop)
+        {
+            speed_y *= -1;
+            timer = 0f;
+            timeToStop = false;
+        }
+    }
+
+    public void StopAtBottom()
+    {
+        timer += Time.deltaTime;
+        timeToStop = true;
+        if (timer >= HamsterController.delayBottom)
+        {
+            speed_y *= -1;
+            timer = 0f;
+            timeToStop = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (hamsterIsClickable == true)
+        {
+            HamsterController.score += 10;
+            hamsterIsClickable = false;
+        }
+    }
+}
